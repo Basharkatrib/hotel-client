4551c1ed9bfaead7a611a81ad34f3b0f8f8691e5
BasharKatrib <qlb2626@gmail.com>
2026-01-08 21:58:07 +0300

    test server8

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react'
-import { HashRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -21,9 +20,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <HashRouter>
-          <App />
-        </HashRouter>
+        <App />
       </PersistGate>
     </Provider>
   </StrictMode>,
