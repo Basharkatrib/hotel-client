652cb8d3210ed00798c00fdd449734718d256fff
BasharKatrib <qlb2626@gmail.com>
2026-03-22 16:40:16 +0300

    Add chat bot ai2

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../common/Navbar/index';
 import Footer from '../common/Footer/index';
-import PromotionsBar from './PromotionsBar';
 
 const MainLayout = () => {
   return (
     <div className="relative">
-      <PromotionsBar />
       <Navbar />
       <Outlet />
       <Footer />
